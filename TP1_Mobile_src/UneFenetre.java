dd25fee59097308beb33f416631b3f639ec7012a
Maxime Bognon <22201386@me.uvsq.fr>
2024-09-27 12:24:58 +0200

    ajout du bouton start/stop main non fonctionel

--- TP1_Mobile_src/UneFenetre.java
+++ TP1_Mobile_src/UneFenetre.java
@@ -6,6 +6,7 @@ import javax.swing.*;
 class UneFenetre extends JFrame 
 {
     UnMobile sonMobile;
+    JButton sonBouton;
     private final int LARG=400, HAUT=250;
     
     public UneFenetre()
@@ -13,7 +14,10 @@ class UneFenetre extends JFrame
     super("le mobile");
     Container leContainer = getContentPane();
     sonMobile = new UnMobile(LARG, HAUT);
+    sonBouton = new JButton("Start/Stop");
+    sonBouton.setPreferredSize(new Dimension(10, 10));
     leContainer.add(sonMobile);
+    leContainer.add(sonBouton);
     Thread laTache = new Thread(sonMobile);
     laTache.start();
     setSize(LARG, HAUT);
