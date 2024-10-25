243e61861b9ab7b366c7ec5726fc33222574d744
Maxime Bognon <22201386@me.uvsq.fr>
2024-10-25 09:28:54 +0200

    Solution affichage en connaissant la ressource critique (System.out)

--- TP2_Affichage/Affichage.java
+++ TP2_Affichage/Affichage.java
@@ -7,17 +7,15 @@ import java.io.Reader;
 import java.io.BufferedReader;
 import java.lang.String;
 
-class Exclusion{};
+
 public class Affichage extends Thread{
 	String texte; 
         
-     static Exclusion exclusionMutuelle = new Exclusion();
-
 	public Affichage (String txt){texte=txt;}
 	
 	public void run(){
 
-	    synchronized (exclusionMutuelle) { //section critique
+	    synchronized (System.out) { //section critique
 	    for (int i=0; i<texte.length(); i++){
 		    System.out.print(texte.charAt(i));
 		    try {sleep(100);} catch(InterruptedException e){};
